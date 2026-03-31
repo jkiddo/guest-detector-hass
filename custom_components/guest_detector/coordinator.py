010d290e94e009e4bb4ff8a170a685b79ba93bc0
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2026-03-31 22:33:34 +0200

    fixing types

--- custom_components/guest_detector/coordinator.py
+++ custom_components/guest_detector/coordinator.py
@@ -225,14 +225,13 @@ class EnergyWindowCoordinator(DataUpdateCoordinator):
         for entry in entity_stats:
             day_value = entry.get("change")
             if day_value is not None and day_value > 0:
-                raw_start = entry.get("start")
-                if raw_start is None:
-                    continue
-                start_dt = (
-                    raw_start
-                    if isinstance(raw_start, datetime)
-                    else dt_util.parse_datetime(str(raw_start))
-                )
+                raw_start = entry["start"]
+                if isinstance(raw_start, datetime):
+                    start_dt = raw_start
+                elif isinstance(raw_start, (int, float)):
+                    start_dt = dt_util.utc_from_timestamp(raw_start)
+                else:
+                    start_dt = dt_util.parse_datetime(raw_start)
                 daily_values.append(
                     {"date": start_dt, "energy": round(day_value, 2)}
                 )
